c094375d691c25028fb4ffb3c95ffd50fedef1b0
Daniela Sannino <danielasannino@Danielas-MacBook-Pro.local>
2018-02-23 10:47:34 -0500

    added forEach to animatePoints

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,13 +1,26 @@
-var animatePoints = function() {
-  var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
-  for (var i = 0; i < points.length; i++) {
-    var revealPoint = function() {
-      points[i].style.opacity = 1;
-      points[i].style.transform = "scaleX(1) translateY(0)";
-      points[i].style.msTransform = "scaleX(1) translateY(0)";
-      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-    revealPoint(i);
-  }
+var revealPoint = function(point) {
+  point.style.opacity = 1;
+  point.style.transform = "scaleX(1) translateY(0)";
+  point.style.msTransform = "scaleX(1) translateY(0)";
+  point.style.WebkitTransform = "scaleX(1) translateY(0)";
 };
+var animatePoints = function(points) {
+    forEach(points,revealPoints);
+  };
+
+window.onload = function() {
+  // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray)
+  }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+  });
+}
